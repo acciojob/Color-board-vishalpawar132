3b7d94284b5cb2307b3f62f51ba8aa2e979ef999
vishalpawar132 <133097620+vishalpawar132@users.noreply.github.com>
2023-05-31 09:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+    // Generate 800 boxes
+    for (let i = 0; i < 800; i++) {
+      const box = document.createElement("div");
+      box.classList.add("square");
+
+      // Change background color on hover
+      box.addEventListener("mouseenter", function() {
+        box.style.backgroundColor = "blue";
+      });
+
+      // Transition back to original color after 1 second
+      box.addEventListener("mouseleave", function() {
+        setTimeout(function() {
+          box.style.backgroundColor = "lightgray";
+        }, 1000);
+      });
+
+      container.appendChild(box);
+    }
